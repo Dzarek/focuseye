d837b12e4f1d34a5a01d037338d40d57cb94c20a
Jarek <jarekjanas95@gmail.com>
2022-07-13 21:19:02 +0200

    first-commit

---
+++ public/data.js
@@ -0,0 +1,248 @@
+const ciaza1 = "./images/gallery/ciążowe/1.jpg";
+const ciaza2 = "./images/gallery/ciążowe/2.jpg";
+const ciaza3 = "./images/gallery/ciążowe/3.jpg";
+const ciaza4 = "./images/gallery/ciążowe/4.jpg";
+const ciaza5 = "./images/gallery/ciążowe/5.jpg";
+const ciaza6 = "./images/gallery/ciążowe/6.jpg";
+const ciaza7 = "./images/gallery/ciążowe/7.jpg";
+const ciaza8 = "./images/gallery/ciążowe/8.jpg";
+const ciaza9 = "./images/gallery/ciążowe/9.jpg";
+const ciaza10 = "./images/gallery/ciążowe/10.jpg";
+const ciaza11 = "./images/gallery/ciążowe/11.jpg";
+const ciaza12 = "./images/gallery/ciążowe/12.jpg";
+const ciaza13 = "./images/gallery/ciążowe/13.jpg";
+
+const moda1 = "./images/gallery/moda/62.jpg";
+const moda2 = "./images/gallery/moda/63.jpg";
+const moda3 = "./images/gallery/moda/64.jpg";
+const moda4 = "./images/gallery/moda/65.jpg";
+const moda5 = "./images/gallery/moda/66.jpg";
+const moda6 = "./images/gallery/moda/67.jpg";
+
+const portret1 = "./images/gallery/portety/92.jpg";
+const portret2 = "./images/gallery/portety/94.jpg";
+const portret3 = "./images/gallery/portety/96.jpg";
+const portret4 = "./images/gallery/portety/112.jpg";
+const portret5 = "./images/gallery/portety/116.jpg";
+
+const rodzinne1 = "./images/gallery/rodzinne/98.jpg";
+const rodzinne2 = "./images/gallery/rodzinne/99.jpg";
+const rodzinne3 = "./images/gallery/rodzinne/100.jpg";
+const rodzinne4 = "./images/gallery/rodzinne/101.jpg";
+const rodzinne5 = "./images/gallery/rodzinne/102.jpg";
+const rodzinne6 = "./images/gallery/rodzinne/103.jpg";
+const rodzinne7 = "./images/gallery/rodzinne/104.jpg";
+const rodzinne8 = "./images/gallery/rodzinne/105.jpg";
+const rodzinne9 = "./images/gallery/rodzinne/106.jpg";
+const rodzinne10 = "./images/gallery/rodzinne/107.jpg";
+const rodzinne11 = "./images/gallery/rodzinne/108.jpg";
+const rodzinne12 = "./images/gallery/rodzinne/109.jpg";
+
+const slubne1 = "./images/gallery/ślubne/1.jpg";
+const slubne2 = "./images/gallery/ślubne/2.jpg";
+const slubne3 = "./images/gallery/ślubne/3.jpg";
+const slubne4 = "./images/gallery/ślubne/4.jpg";
+const slubne5 = "./images/gallery/ślubne/5.jpg";
+const slubne6 = "./images/gallery/ślubne/7.jpg";
+const slubne7 = "./images/gallery/ślubne/6.jpg";
+const slubne8 = "./images/gallery/ślubne/8.jpg";
+const slubne9 = "./images/gallery/ślubne/9.jpg";
+const slubne10 = "./images/gallery/ślubne/10.jpg";
+const slubne11 = "./images/gallery/ślubne/11.jpg";
+
+const headerCiaza = "./images/header/headerCiaza.jpg";
+const headerRodzinne = "./images/header/headerRodzinne.jpg";
+const headerSlub = "./images/header/headerSlub.jpg";
+const headerCiazaM = "./images/header/headerCiazaM.jpg";
+const headerModaM = "./images/header/headerModaM.jpg";
+const headerPortfolioM = "./images/header/headerPortfolioM.jpg";
+const headerRodzinneM = "./images/header/headerRodzinneM.jpg";
+const headerSlubM = "./images/header/headerSlubM.jpg";
+
+export const gallery = [
+  {
+    id: 1,
+    img: [
+      ciaza1,
+      ciaza2,
+      ciaza3,
+      ciaza4,
+      ciaza5,
+      ciaza6,
+      ciaza7,
+      ciaza8,
+      ciaza9,
+      ciaza10,
+      ciaza11,
+      ciaza12,
+      ciaza13,
+    ],
+    category: "ciąża",
+  },
+  {
+    id: 2,
+    img: [moda1, moda2, moda3, moda4, moda5, moda6],
+    category: "moda",
+  },
+  {
+    id: 3,
+    img: [portret1, portret2, portret3, portret4, portret5],
+    category: "portrety",
+  },
+  {
+    id: 4,
+    img: [
+      rodzinne1,
+      rodzinne2,
+      rodzinne3,
+      rodzinne4,
+      rodzinne5,
+      rodzinne6,
+      rodzinne7,
+      rodzinne8,
+      rodzinne9,
+      rodzinne10,
+      rodzinne11,
+      rodzinne12,
+    ],
+    category: "rodzinne",
+  },
+  {
+    id: 5,
+    img: [
+      slubne1,
+      slubne2,
+      slubne3,
+      slubne4,
+      slubne5,
+      slubne6,
+      slubne7,
+      slubne8,
+      slubne9,
+      slubne10,
+      slubne11,
+    ],
+    category: "ślubne",
+  },
+];
+
+export const offers = [
+  {
+    id: 1,
+    category: "ciążowe",
+    title: "Sesja Ciążowa",
+    price: 200,
+    numberOfImages: 20,
+    time: "2h",
+    place: "plener,dom",
+    ready: "do 3 tygodni",
+    album: "za dodatkową opłatą",
+    quality: "Full HD",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ipsam aspernatur cumque praesentium ratione tempora officia sint sit adipisci expedita, ut animi aliquam nihil. Alias totam ut consequuntur ducimus corrupti? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ipsam aspernatur cumque praesentium ratione tempora officia sint sit adipisci expedita, ut animi aliquam nihil. Alias totam ut consequuntur ducimus corrupti?",
+    imgs: [ciaza13, ciaza10],
+  },
+  {
+    id: 2,
+    category: "moda",
+    title: "Sesja Mody",
+    price: 300,
+    numberOfImages: 30,
+    time: "3h",
+    place: "plener,dom",
+    ready: "do 5 tygodni",
+    album: "za dodatkową opłatą",
+    quality: "Full HD",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ipsam aspernatur cumque praesentium ratione tempora officia sint sit adipisci expedita, ut animi aliquam nihil. Alias totam ut consequuntur ducimus corrupti? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ipsam aspernatur cumque praesentium ratione tempora officia sint sit adipisci expedita, ut animi aliquam nihil. Alias totam ut consequuntur ducimus corrupti?",
+    imgs: [moda2, moda5],
+  },
+  {
+    id: 3,
+    category: "portrety",
+    title: "Sesja Portretowa",
+    price: 100,
+    numberOfImages: 10,
+    time: "1h",
+    place: "plener,dom",
+    ready: "do 2 tygodni",
+    album: "za dodatkową opłatą",
+    quality: "Full HD",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ipsam aspernatur cumque praesentium ratione tempora officia sint sit adipisci expedita, ut animi aliquam nihil. Alias totam ut consequuntur ducimus corrupti? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ipsam aspernatur cumque praesentium ratione tempora officia sint sit adipisci expedita, ut animi aliquam nihil. Alias totam ut consequuntur ducimus corrupti?",
+    imgs: [portret2, portret1],
+  },
+  {
+    id: 4,
+    category: "rodzinne",
+    title: "Sesja Rodzinna",
+    price: 300,
+    numberOfImages: 30,
+    time: "2h",
+    place: "plener,dom",
+    ready: "do 4 tygodni",
+    album: "za dodatkową opłatą",
+    quality: "Full HD",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ipsam aspernatur cumque praesentium ratione tempora officia sint sit adipisci expedita, ut animi aliquam nihil. Alias totam ut consequuntur ducimus corrupti? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ipsam aspernatur cumque praesentium ratione tempora officia sint sit adipisci expedita, ut animi aliquam nihil. Alias totam ut consequuntur ducimus corrupti?",
+    imgs: [rodzinne1, rodzinne7],
+  },
+  {
+    id: 5,
+    category: "ślubne",
+    title: "Sesja Ślubna",
+    price: 500,
+    numberOfImages: 100,
+    time: "4h",
+    place: "plener",
+    ready: "do 6 tygodni",
+    album: "za dodatkową opłatą",
+    quality: "Full HD",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ipsam aspernatur cumque praesentium ratione tempora officia sint sit adipisci expedita, ut animi aliquam nihil. Alias totam ut consequuntur ducimus corrupti? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ipsam aspernatur cumque praesentium ratione tempora officia sint sit adipisci expedita, ut animi aliquam nihil. Alias totam ut consequuntur ducimus corrupti?",
+    imgs: [slubne6, slubne9],
+  },
+];
+
+export const headerData = [
+  {
+    id: 1,
+    img: headerCiaza,
+    imgM: headerCiazaM,
+    shortTitle: "sesje ciążowe",
+    longTitle: "Sesja ciążowa dla Ciebie i Twojego brzuszka",
+    category: "ciążowe",
+  },
+  {
+    id: 2,
+    img: headerRodzinne,
+    imgM: headerRodzinneM,
+    shortTitle: "sesje rodzinne",
+    longTitle: "Sesja zdjęciowa z Twoimi najbliższymi",
+    category: "rodzinne",
+  },
+  {
+    id: 3,
+    img: headerSlub,
+    imgM: headerSlubM,
+    shortTitle: "sesje ślubne",
+    longTitle: "Romantyczna sesja ślubna",
+    category: "ślubne",
+  },
+  {
+    id: 4,
+    img: portret4,
+    imgM: headerPortfolioM,
+    shortTitle: "zdjęcia portretowe",
+    longTitle: "Zdjęcia do Twojego CV i portfolio",
+    category: "portrety",
+  },
+  {
+    id: 5,
+    img: moda3,
+    imgM: headerModaM,
+    shortTitle: "sesje mody",
+    longTitle: "Pokaż siebie i swój styl",
+    category: "moda",
+  },
+];
