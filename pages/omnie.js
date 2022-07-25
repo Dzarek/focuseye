c264f843e50a508478e1ca49bc44d2d4aa2561be
Jarek <jarekjanas95@gmail.com>
2022-07-25 03:05:39 +0200

    about me

--- pages/omnie.js
+++ pages/omnie.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import Aos from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
+import { FaLaughBeam, FaGrinHearts } from "react-icons/fa";
 
 const aboutMeHeaderImg = "../images/aboutMeImg/aboutMeHeader.png";
 const meImg2 = "../images/aboutMeImg/meImg2.jpg";
@@ -35,24 +36,49 @@ const OmniePage = () => {
           </p>
         </div>
       </header>
+      <h2 className="aboutTitle">Drobna fotografka z wrażliwym sercem</h2>
       <div className="aboutMeContent">
         <img src={meImg2} alt="ja z rodziną" />
         <section>
-          <h2>Hejka!</h2>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea repellat
-            quisquam pariatur debitis. Vero odit eligendi, molestias earum, esse
-            doloremque recusandae ex deserunt molestiae doloribus expedita rem
-            sed hic, accusantium quia voluptates sit sapiente. Ea totam nesciunt
-            possimus, adipisci at repellat, illo officia cupiditate culpa
-            dolorem ad. At ex sequi perspiciatis ut provident expedita
-            dignissimos dolore enim dolorem doloribus, a labore consequatur,
-            repellat voluptatem ipsa! Libero dolor maxime reiciendis placeat
-            perspiciatis eos, earum exercitationem autem praesentium! Provident
-            magni ipsam excepturi reprehenderit tenetur consequatur quasi,
-            ratione rem quia esse iure aliquid voluptate natus. Dicta quis,
-            aperiam voluptatem porro architecto rerum suscipit?
+            Chodząc jeszcze do liceum, byłam przekonana, że kura ma 4 nogi i
+            jakież było moje zdziwienie gdy nieopodal przystanku autobusowego
+            zobaczyłam biedną kurę o dwóch nogach… wyobraźcie sobie minę mojej
+            koleżanki Elizy <FaLaughBeam />.
           </p>
+          <p>
+            We wczesnym dzieciństwie chowałam się pod Fiatem 126P taplając się w
+            błotku! Z perspektywy czasu sądzę, że już wtedy miałam zadatki na
+            fotografa outdoorowego…
+          </p>
+          <p>
+            Uważam, że lepszej drugiej połówki niż mój mąż, nie mogłam sobie
+            wymarzyć. Od lat wspieramy się w swoich pasjach. Można by
+            powiedzieć, że rower i aparat to takie nasze drugie małżeństwo{" "}
+            <FaGrinHearts />.
+          </p>
+          <p>
+            W fotografii zależy mi na tym aby oddawać Waszą naturalną
+            wrażliwość, delikatność i subtelność. Cudnie gdy zapraszacie mnie do
+            swoich domów, sesje lifestylowe to jest to co kocham najbardziej, a
+            fotografia plenerowa to dla mnie przysłowiowa wisienka na torcie!
+          </p>
+        </section>
+      </div>
+      <div className="aboutNumbers">
+        <section data-aos="fade-right">
+          <span>150</span>
+          <h4>sesji zdjęciowych</h4>
+        </section>
+        <div className="lineNumber"></div>
+        <section data-aos="fade-up">
+          <span>360</span>
+          <h4>zadowolonych klientów</h4>
+        </section>
+        <div className="lineNumber"></div>
+        <section data-aos="fade-left">
+          <span>10</span>
+          <h4>lat doświadczenia</h4>
         </section>
       </div>
     </Wrapper>
@@ -131,19 +157,70 @@ const Wrapper = styled.div`
       }
     }
   }
+  .aboutTitle {
+    color: var(--secondaryColor);
+    font-size: 1.8rem;
+    margin: 15vh auto 7vh;
+    text-align: center;
+    text-transform: uppercase;
+    font-weight: 500;
+  }
   .aboutMeContent {
     width: 90vw;
-    margin: 20vh auto;
+    margin: 0 auto 15vh;
     display: flex;
     justify-content: space-around;
     align-items: center;
-
     img {
       width: 50%;
     }
+
+    section {
+      width: 40%;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      align-items: flex-start;
+      p {
+        font-size: 1.2rem;
+        margin-bottom: 3vh;
+        svg {
+          color: var(--secondaryColor);
+          margin-left: 5px;
+          font-size: 1.1rem;
+        }
+        :nth-last-of-type(1) {
+          margin-bottom: 0;
+        }
+      }
+    }
   }
-  section {
-    width: 40%;
+  .aboutNumbers {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 55vw;
+    margin: 10vh auto 15vh;
+    section {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      span {
+        color: var(--secondaryColor);
+        font-size: 3rem;
+      }
+      h4 {
+        font-family: var(--titleFont);
+        font-size: 1rem;
+        font-weight: 500;
+      }
+    }
+    .lineNumber {
+      width: 2px;
+      height: 50px;
+      background: var(--secondaryColor3);
+    }
   }
 `;
 
