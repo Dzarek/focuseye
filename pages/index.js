7b9f4683a3e0c6b225c8368a68751c3bd0f40295
Jarek <jarekjanas95@gmail.com>
2022-08-10 22:32:13 +0200

    home page ready

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Header from "../components/homePage/Header";
 import FirstInfo from "../components/homePage/FirstInfo";
@@ -12,8 +12,50 @@ import ShortFaqContact from "../components/homePage/ShortFaqContact";
 import ShortInstagram from "../components/homePage/ShortInstagram";
 // import { blogData } from "../public/data";
 
-const Home = ({ thumbnails, fourTitle, offersWP }) => {
+const Home = () => {
   const [showDetails, setShowDetails] = useState("");
+  const [thumbnails, showThumbnails] = useState(null);
+  const [fourTitle, showFourTitle] = useState(null);
+  const [offersWP, showOffersWP] = useState(null);
+
+  useEffect(async () => {
+    try {
+      const response = await fetch(
+        "https://focuseye.pl/wp-json/wp/v2/media?media_folder=59&per_page=100"
+      );
+      const data = await response.json();
+      const thumbnails = data.map((image) => {
+        const smallImg = image.media_details.sizes.full.source_url;
+        return smallImg;
+      });
+
+      const responseBlog = await fetch(
+        `https://focuseye.pl/wp-json/wp/v2/artykuly`
+      );
+      const dataBlog = await responseBlog.json();
+      const blogWP = dataBlog.map((article) => {
+        return article;
+      });
+      const titleOfArticles = blogWP.map((item) => {
+        const title = item.acf.title;
+        return title;
+      });
+      const fourTitle = titleOfArticles.slice(0, 4);
+
+      const responseOferta = await fetch(
+        `https://focuseye.pl/wp-json/wp/v2/oferty?order=asc`
+      );
+      const dataOfferWP = await responseOferta.json();
+      const offersWP = dataOfferWP.map((offer) => {
+        return offer;
+      });
+      showThumbnails(thumbnails);
+      showFourTitle(fourTitle);
+      showOffersWP(offersWP);
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
 
   return (
     <>
@@ -57,51 +99,51 @@ const Home = ({ thumbnails, fourTitle, offersWP }) => {
   );
 };
 
-export const getStaticProps = async () => {
-  try {
-    const response = await fetch(
-      "https://focuseye.pl/wp-json/wp/v2/media?media_folder=59&per_page=100"
-    );
-    const data = await response.json();
-    const thumbnails = data.map((image) => {
-      const smallImg = image.media_details.sizes.full.source_url;
-      return smallImg;
-    });
+// export const getStaticProps = async () => {
+//   try {
+//     const response = await fetch(
+//       "https://focuseye.pl/wp-json/wp/v2/media?media_folder=59&per_page=100"
+//     );
+//     const data = await response.json();
+//     const thumbnails = data.map((image) => {
+//       const smallImg = image.media_details.sizes.full.source_url;
+//       return smallImg;
+//     });
 
-    const responseBlog = await fetch(
-      `https://focuseye.pl/wp-json/wp/v2/artykuly`
-    );
-    const dataBlog = await responseBlog.json();
-    const blogWP = dataBlog.map((article) => {
-      return article;
-    });
-    const titleOfArticles = blogWP.map((item) => {
-      const title = item.acf.title;
-      return title;
-    });
-    const fourTitle = titleOfArticles.slice(0, 4);
+//     const responseBlog = await fetch(
+//       `https://focuseye.pl/wp-json/wp/v2/artykuly`
+//     );
+//     const dataBlog = await responseBlog.json();
+//     const blogWP = dataBlog.map((article) => {
+//       return article;
+//     });
+//     const titleOfArticles = blogWP.map((item) => {
+//       const title = item.acf.title;
+//       return title;
+//     });
+//     const fourTitle = titleOfArticles.slice(0, 4);
 
-    const responseOferta = await fetch(
-      `https://focuseye.pl/wp-json/wp/v2/oferty?order=asc`
-    );
-    const dataOfferWP = await responseOferta.json();
-    const offersWP = dataOfferWP.map((offer) => {
-      return offer;
-    });
+//     const responseOferta = await fetch(
+//       `https://focuseye.pl/wp-json/wp/v2/oferty?order=asc`
+//     );
+//     const dataOfferWP = await responseOferta.json();
+//     const offersWP = dataOfferWP.map((offer) => {
+//       return offer;
+//     });
 
-    return {
-      props: {
-        thumbnails,
-        fourTitle,
-        offersWP,
-      },
-      revalidate: 60,
-    };
-  } catch (error) {
-    return {
-      props: {},
-    };
-  }
-};
+//     return {
+//       props: {
+//         thumbnails,
+//         fourTitle,
+//         offersWP,
+//       },
+//       revalidate: 60,
+//     };
+//   } catch (error) {
+//     return {
+//       props: {},
+//     };
+//   }
+// };
 
 export default Home;
